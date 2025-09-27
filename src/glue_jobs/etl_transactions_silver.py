4dd6edaa307d843622eef5279a48bd4eafe8c37d
al-jpacho <jordanapacho@gmail.com>
2025-09-27 13:38:03 +0100

    chore(etl-silver): add glue boilerplate

---
+++ src/glue_jobs/etl_transactions_silver.py
@@ -0,0 +1,17 @@
+import sys
+
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from pyspark.sql import functions as F
+from pyspark.sql.types import DecimalType
+from pyspark.sql.window import Window
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
